741a4af16178b7ad02161ab9a6932f867a4352db
base8314 <jsbasedow@gmail.com>
2024-11-30 10:37:00 -0500

    2 Draw Player Triangle

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -15,6 +15,8 @@ def main():
     #Setting Resolution of screen
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
     
+    game_clock = pygame.time.Clock()
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -22,10 +24,14 @@ def main():
 
         #Fill Screen With Black
         screen.fill((0,0,0))
+        player= Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
 
         #update the display
+        player.draw(screen)
         pygame.display.flip()
-
+        
+        game_clock.tick(FPS)
+        dt = game_clock.get_time()/1000
 
 if __name__== "__main__":
     main()
