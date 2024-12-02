f2fcec471e9ff9a08b6b3c1a28f09c6a758e0f6b
base8314 <jsbasedow@gmail.com>
2024-12-02 18:52:35 -0500

    Player Movement and groups

--- main.py
+++ main.py
@@ -12,10 +12,16 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT }")
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+
     #Setting Resolution of screen
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH,SCREEN_HEIGHT))
-    
+    player= Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
     game_clock = pygame.time.Clock()
+    dt = 0
 
     while running:
         for event in pygame.event.get():
@@ -24,10 +30,17 @@ def main():
 
         #Fill Screen With Black
         screen.fill((0,0,0))
-        player= Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2,PLAYER_RADIUS)
+        
 
         #update the display
-        player.draw(screen)
+        
+        
+        for updates in updatable:
+            updates.update(dt)
+        
+        for drawings in drawable:
+            drawings.draw(screen)
+        
         pygame.display.flip()
         
         game_clock.tick(FPS)
